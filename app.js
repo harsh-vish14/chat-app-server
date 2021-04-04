9dbc4bd65322a60eaa8e84006affbe40ad5fe5b1
harsh-vish14 <harshkv9322@gmail.com>
2021-04-04 21:26:27 +0530

    Added basic server

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+require('dotenv').config();
+const socket = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 4000;
+const app = express();
+const server = http.createServer(app);
+const firebase = require('firebase');
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID
+};
+const firebaseApp = firebase.initializeApp(config);
+const db = firebaseApp.firestore()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+})
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('user joint')
+    socket.on('chat-input', (data) => {
+        socket.broadcast.emit(`message-came-${data.id}`, data.message);
+    })
+    socket.on('new-users-entered', (data) => {
+        socket.broadcast.emit(`chat-${data.chatId} `, data);
+        
+        console.log(data);
+    })
+    socket.on('disconnect', () => {
+        console.log('user joint disconnected');
+    })
+
+})
+
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'hello from server new update'
+    })
+})
+
+
+server.listen(PORT, () => {
+    console.log(`server is on port ${PORT}`);
+})
